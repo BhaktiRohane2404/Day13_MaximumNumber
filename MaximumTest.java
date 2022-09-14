f39b57981c9ad8217273ae7a634118fcc15d947f
BhaktiRohane2404 <98884739+BhaktiRohane2404@users.noreply.github.com>
2022-09-14 19:25:48 +0530

    To Find Maximum Numbers

---
+++ MaximumTest.java
@@ -0,0 +1,29 @@
+package com.bridgelabz.Generics;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class MaximumTest {
+    @Test
+    public void intMax() {
+        int actualValue = Maximum.testMaximum(9, 4, 1);
+        Assert.assertEquals(9, actualValue);
+    }
+
+    @Test
+    public void floatMax() {
+        float actualValue = Maximum.testMaximum(1.5f,9.1f,4.4f);
+        Assert.assertEquals(9.1f,actualValue,0);
+    }
+
+    @Test
+    public void stringMax() {
+        String actualValue = Maximum.testMaximum("apple","peach","banana");
+        Assert.assertEquals("peach",actualValue);
+    }
+    @Test
+    public void givenStringValues_WhenGetMaximum_ShouldReturnCorrectValue() {
+        String actualResult = Maximum.testMaximum("Apple", "Peach", "Banana", "Mango","Strawberry");
+        Assert.assertEquals("Strawberry", actualResult);
+    }
+}
